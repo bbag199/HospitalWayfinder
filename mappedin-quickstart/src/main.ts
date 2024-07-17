f401969d20561166929fe5727b0f750358220b7a
bbag199 <148782320+bbag199@users.noreply.github.com>
2024-07-18 08:34:04 +1200

    Added interactive spaces and labels for each space

--- mappedin-quickstart/src/main.ts
+++ mappedin-quickstart/src/main.ts
@@ -12,6 +12,70 @@ const options = {
 async function init() {
   const mapData = await getMapData(options);
   const mapView = await show3dMap(document.getElementById('mappedin-map') as HTMLDivElement, mapData);
+
+  //add interactive spaces
+  mapData.getByType('space').forEach((space) => {
+    mapView.updateState(space, {
+      interactive: true,
+      hoverColor: "orange",
+    })
+  })
+
+  //add a label for each space
+  mapData.getByType('space').forEach((space) => {
+    if(space.name){
+      mapView.Labels.add(space, space.name, {
+        appearance: {
+          text: {foregroundColor: "orange"}, 
+        },
+      });
+    }
+  });
+
+  mapView.on("click", async (event) => {
+    if (event.markers.length > 0) {
+      console.log("Clicked on marker: " + event.markers[0].id);
+      mapView.Markers.remove(event.markers[0]);
+    } else {
+      if (event.spaces.length > 0 && event.spaces[0].name) {
+        const markerTemplate = `
+                <div>
+                  <style>
+                  .marker {
+                    display: flex;
+                    align-items: center;
+                    background-color: #fff;
+                    max-height: 64px;
+                    border: 2px solid grey;
+                    padding: 4px 12px;
+                    font-weight: bold;
+                    font-family: sans-serif;
+                  }
+      
+                  .marker img {
+                    max-width: 64px;
+                    max-height: 32px;
+                    object-fit: contain;
+                    margin-right: 12px;
+                  }
+                  </style>
+                  <div class="marker">
+                    <p>${event.spaces[0].name}</p>
+                  </div>
+                </div>`;
+
+        mapView.Markers.add(event.coordinate, markerTemplate, {
+          interactive: true,
+          anchor: "left",
+        });
+      } else {
+        mapView.Markers.add(event.coordinate, "<div>Unnamed Space</div>", {
+          interactive: true,
+          anchor: "right",
+        });
+      }
+    }
+  });
 }
 
 init();
