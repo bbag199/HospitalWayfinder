061f6c12da6b19e08b726b5affd78bc704e5f933
Jovana Stefanovic <jovanas384@gmail.com>
2024-07-22 13:28:34 +1200

    Added settings button, search, rotation,

--- mappedin-quickstart/src/main.ts
+++ mappedin-quickstart/src/main.ts
@@ -1,9 +1,10 @@
-import { getMapData, show3dMap, MapView, Space, Path } from "@mappedin/mappedin-js";
+import { getMapData, show3dMap, MapView, Space, Path, Coordinate } from "@mappedin/mappedin-js";
 import "@mappedin/mappedin-js/lib/index.css";
 
 // See Trial API key Terms and Conditions
 // https://developer.mappedin.com/web/v6/trial-keys-and-maps/
 const options = {
+
   key: '6666f9ba8de671000ba55c63',
   secret: 'd15feef7e3c14bf6d03d76035aedfa36daae07606927190be3d4ea4816ad0e80',
   mapId: '6637fd20269972f02bf839da',
@@ -81,6 +82,33 @@ async function init() {
       path = null;
     }
   });
+
+  // Set the camera position with final bearing and zoom level
+  const setCameraPosition = () => {
+    const entranceCoordinate = new Coordinate(-37.007839, 174.888214); // Replace with actual coordinates
+
+    // Set the camera position with final bearing and zoom level
+    mapView.Camera.animateTo(
+      {
+        bearing: 167.5 + 10, // Total rotation of 177.5 degrees (167.5 + 10)
+        pitch: 80,
+        zoomLevel: 300, // Increase zoom level to zoom out further
+        center: entranceCoordinate,
+      },
+      { duration: 2000 } // Set duration to 0 for an instant move
+    );
+  };
+
+  // Call the function to set the camera position
+  setCameraPosition();
+
+  // Iterate through each point of interest and label it.
+for (const poi of mapData.getByType('point-of-interest')) {
+	// Label the point of interest if it's on the map floor currently shown.
+	if (poi.floor.id === mapView.currentFloor.id) {
+		mapView.Labels.add(poi.coordinate, poi.name);
+	}
+}
 
   console.log(mapData.getByType("floor"));
 }
