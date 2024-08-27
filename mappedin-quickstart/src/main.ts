34effee98005a28ed92da8a53e0fda410e131322
Jovana-stack <jovanas384@gmail.com>
2024-08-27 23:08:20 +1200

    Clear route

--- mappedin-quickstart/src/main.ts
+++ mappedin-quickstart/src/main.ts
@@ -105,6 +105,9 @@ async function init() {
             if (navigationState.isPathDrawn) {
                 mapView.Paths.removeAll();
                 mapView.Markers.removeAll();
+                navigationState.endSpace = event.spaces[0];
+                navigationState.startSpace = event.spaces[0];
+                
                 navigationState.isPathDrawn = false;
             }
   
@@ -251,8 +254,7 @@ floorSelector.value = mapView.currentFloor.id;
 floorSelector.addEventListener("change", (e) => {
   mapView.setFloor((e.target as HTMLSelectElement)?.value);
 });
-
-  //Add the Stack Map and testing:
+//Add the Stack Map and testing:
   //1)Add the stack "enable button":
   const stackMapButton = document.createElement("button");
   // Add any classes, text, or other properties (these two code can be linked to the css file):
@@ -263,12 +265,17 @@ floorSelector.addEventListener("change", (e) => {
   mappedinDiv.appendChild(stackMapButton);
 
   //Testing: no show floors:
-  //Find the floor that need to do the Stack Map, at this case, we testing 
-  const noShowFloor2: Floor[] = mapData.getByType("floor").filter((floor: Floor) => (floor.name !== "Level 1" && floor.name !== "Ground floor"));
+  //Find the floor that need to do the Stack Map, at this case, we testing
+  const noShowFloor2: Floor[] = mapData
+    .getByType("floor")
+    .filter(
+      (floor: Floor) =>
+        floor.name !== "SuperClinic Level 1" &&
+        floor.name !== "SuperClinic & Surgical Centre Ground Lvl"
+    );
 
   // The enable Button is used to enable and disable Stacked Maps.
   stackMapButton.onclick = () => {
-    
     //debug here:
     console.log("Chekcing noShowFloor2", noShowFloor2);
     //show the stack map here and hide the no used floor:
@@ -278,28 +285,35 @@ floorSelector.addEventListener("change", (e) => {
       // Show the stack map and hide the unused floor
       mapView.expand({ excludeFloors: noShowFloor2 });
       stackMapButton.textContent = "Disable Stack Map";
+
+      // Set the camera to zoomLevel 17 and pitch 0
+      mapView.Camera.animateTo({
+        bearing: floorSettings[mapView.currentFloor.id].bearing, //178.5  // set the angle, e.g. North or South facing
+        zoomLevel: 18.7, // set the zoom level, better in 17-22
+        pitch: 85,      // the angle from the top-down (0: Top-down, 90: Eye-level)   
+      });
+
     } else {
       // Collapse the stack map
       mapView.collapse();
       stackMapButton.textContent = "Enable Stack Map";
+      setCameraPosition(mapView.currentFloor.id);
+      
     }
     
-    //mapView.Camera.animateTo({ zoomLevel: 100 }, { duration: 1000 });
-    mapView.Camera.set({
-      zoomLevel: 19, // set the zoom level, better in 17-22
-      pitch: 78,    // the angle from the top-down (0: Top-down, 90: Eye-level)
-      //bearing: 0    // set the angle, e.g. North or South facing
-    })
-
   };
+
 
   //Emergency exit function:
   //get the exit object (already build a exit01 and exit02 object in the dashboard map):
-  const exitSpace = mapData.getByType('object').find(object => object.name.includes("exit01"));
-  const exitSpace2 = mapData.getByType('object').find(object => object.name.includes("exit02"));
-   
+  const exitSpace = mapData
+    .getByType("object")
+    .find((object) => object.name.includes("exit01"));
+  const exitSpace2 = mapData
+    .getByType("object")
+    .find((object) => object.name.includes("exit02"));
 
-  //add an emergency square button here: 
+  //add an emergency square button here:
   const emergencyButton = document.createElement("button");
   emergencyButton.textContent = "Emergency Exit";
   emergencyButton.style.position = "absolute";
@@ -317,11 +331,10 @@ floorSelector.addEventListener("change", (e) => {
   // Append the button to the map container
   mappedinDiv.appendChild(emergencyButton);
 
-
-  emergencyButton.addEventListener('click', function() {
+  emergencyButton.addEventListener("click", function () {
     console.log("chekcing startSpace input:", startSpace);
     console.log("exit01 space information:", exitSpace);
-    
+
     if (startSpace) {
       if (path) {
         mapView.Paths.remove(path);
@@ -336,26 +349,28 @@ floorSelector.addEventListener("change", (e) => {
       let shortestWayout;
 
       if (directions && directions2) {
-          shortestWayout = directions.distance <= directions2.distance ? directions : directions2;
+        shortestWayout =
+          directions.distance <= directions2.distance
+            ? directions
+            : directions2;
       } else if (directions) {
-          shortestWayout = directions;
+        shortestWayout = directions;
       } else if (directions2) {
-          shortestWayout = directions2;
+        shortestWayout = directions2;
       } else {
-          throw new Error("Both directions are undefined");
+        throw new Error("Both directions are undefined");
       }
 
       //build the shortest wayout here:
-      if (shortestWayout) {   
+      if (shortestWayout) {
         path = mapView.Paths.add(shortestWayout.coordinates, {
           nearRadius: 0.5,
           farRadius: 0.5,
           color: "red",
-          animateDrawing: true, 
+          animateDrawing: true,
           drawDuration: 500,
           animateArrowsOnPath: true,
-          displayArrowsOnPath: true
-
+          displayArrowsOnPath: true,
         });
         // Draw the directions on the map.
         mapView.Navigation.draw(shortestWayout);
@@ -363,8 +378,7 @@ floorSelector.addEventListener("change", (e) => {
     } else {
       console.error("Please select start space locations.");
     }
-  }); 
-
+  });
 
   const allPOIs = mapData.getByType("point-of-interest");
   const currentFloor = mapView.currentFloor.id;
@@ -468,7 +482,7 @@ floorSelector.addEventListener("change", (e) => {
       if (path) {
         mapView.Paths.remove(path);
       }
-      const directions = mapView.getDirections(startSpace, endSpace);
+      const directions = mapView.getDirections(startSpace, endSpace, { accessible: accessibilityEnabled });
       if (directions) {
         path = mapView.Paths.add(directions.coordinates, {
           nearRadius: 0.5,
@@ -481,167 +495,173 @@ floorSelector.addEventListener("change", (e) => {
     }
   });
 
-  
-
-// Button Accessibility
-const accessibilityButton = document.createElement("button");
-accessibilityButton.innerHTML = `
+  // Button Accessibility
+  const accessibilityButton = document.createElement("button");
+  accessibilityButton.innerHTML = `
   <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512">
     <path fill="currentColor" d="M192 96a48 48 0 1 0 0-96a48 48 0 1 0 0 96m-71.5 151.2c12.4-4.7 18.7-18.5 14-30.9s-18.5-18.7-30.9-14C43.1 225.1 0 283.5 0 352c0 88.4 71.6 160 160 160c61.2 0 114.3-34.3 141.2-84.7c6.2-11.7 1.8-26.2-9.9-32.5s-26.2-1.8-32.5 9.9C240 440 202.8 464 160 464c-61.9 0-112-50.1-112-112c0-47.9 30.1-88.8 72.5-104.8M259.8 176l-1.9-9.7c-4.5-22.3-24-38.3-46.8-38.3c-30.1 0-52.7 27.5-46.8 57l23.1 115.5c6 29.9 32.2 51.4 62.8 51.4h100.5c6.7 0 12.6 4.1 15 10.4l36.3 96.9c6 16.1 23.8 24.6 40.1 19.1l48-16c16.8-5.6 25.8-23.7 20.2-40.5s-23.7-25.8-40.5-20.2l-18.7 6.2l-25.5-68c-11.7-31.2-41.6-51.9-74.9-51.9h-68.5l-9.6-48H336c17.7 0 32-14.3 32-32s-14.3-32-32-32h-76.2z"/>
   </svg>
 `;
-accessibilityButton.style.position = "absolute";
-accessibilityButton.style.top = "50px";
-accessibilityButton.style.right = "10px";
-accessibilityButton.style.zIndex = "1000";
-accessibilityButton.style.backgroundColor = "#fff";
-accessibilityButton.style.color = "#000";
-accessibilityButton.style.border = "none";
-accessibilityButton.style.padding = "10px";
-accessibilityButton.style.cursor = "pointer";
-accessibilityButton.style.borderRadius = "10px";
+  accessibilityButton.style.position = "absolute";
+  accessibilityButton.style.top = "50px";
+  accessibilityButton.style.right = "10px";
+  accessibilityButton.style.zIndex = "1000";
+  accessibilityButton.style.backgroundColor = "#fff";
+  accessibilityButton.style.color = "#000";
+  accessibilityButton.style.border = "none";
+  accessibilityButton.style.padding = "10px";
+  accessibilityButton.style.cursor = "pointer";
+  accessibilityButton.style.borderRadius = "10px";
 
-mappedinDiv.appendChild(accessibilityButton);
+  mappedinDiv.appendChild(accessibilityButton);
 
-const originalColors: Map<string, string> = new Map();
-let liftsHighlighted = false;
-accessibilityEnabled = false;
+  const originalColors: Map<string, string> = new Map();
+  let liftsHighlighted = false;
+  accessibilityEnabled = false;
 
 accessibilityButton.addEventListener("click", () => {
   accessibilityEnabled = !accessibilityEnabled;
-  const lifts = mapData.getByType("space").filter(space => space.name.toLowerCase().includes("lift"));
-
-  lifts.forEach(lift => {
-    if (!liftsHighlighted) {
-      const originalColor = mapView.getState(lift)?.color;
-      if (originalColor) {
-        originalColors.set(lift.id, originalColor);
-      }
-      mapView.updateState(lift, {
-        color: "#7393B3"
-      });
-    } else {
-      const originalColor = originalColors.get(lift.id);
-      mapView.updateState(lift, {
-        color: originalColor
-      });
-    }
-  });
-
-  liftsHighlighted = !liftsHighlighted;
-  if (liftsHighlighted) {
-    accessibilityButton.style.backgroundColor = "#0f2240";
-    accessibilityButton.style.color = "#fff"
-  } else {
-    accessibilityButton.style.backgroundColor = "#fff";
-    accessibilityButton.style.color = "#000";
-  }
-});
-
-// Icons
-const toiletButton = document.getElementById('toilet-btn') as HTMLButtonElement;
-let toiletActive = false;
-
-toiletButton.addEventListener("click", () => {
-  const toilets = mapData.getByType("space").filter(space => space.name.toLowerCase().includes("toilet"));
-  
-
-  toilets.forEach(toilets => {
-    if (!toiletActive) {
-      const originalColor = mapView.getState(toilets)?.color;
-      if (originalColor) {
-        originalColors.set(toilets.id, originalColor);
-      }
-      mapView.updateState(toilets, {
-        color: "#7393B3"
-      });
-    } else {
-      const originalColor = originalColors.get(toilets.id);
-      mapView.updateState(toilets, {
-        color: originalColor
-      });
-    }
-  });
-
-  toiletActive = !toiletActive;
-  if (toiletActive) {
-    toiletButton.style.backgroundColor = "#0f2240";
-    toiletButton.style.color = "#fff"
-  } else {
-    toiletButton.style.backgroundColor = "#fff";
-    toiletButton.style.color = "#000";
-  }
-});
-
-const cafeButton = document.getElementById('cafe-btn') as HTMLButtonElement;
-let cafeActive = false;
-
-cafeButton.addEventListener("click", () => {
-  const cafes = mapData.getByType("space").filter(space => space.name.toLowerCase().includes("cafe"));
-  
-  cafes.forEach(cafes => {
-    if (!cafeActive) {
-      const originalColor = mapView.getState(cafes)?.color;
-      if (originalColor) {
-        originalColors.set(cafes.id, originalColor);
-      }
-      mapView.updateState(cafes, {
-        color: "#7393B3"
-      });
-    } else {
-      const originalColor = originalColors.get(cafes.id);
-      mapView.updateState(cafes, {
-        color: originalColor
-      });
-    }
-  });
-
-  cafeActive = !cafeActive;
-  if (cafeActive) {
-    cafeButton.style.backgroundColor = "#0f2240";
-    cafeButton.style.color = "#fff"
-  } else {
-    cafeButton.style.backgroundColor = "#fff";
-    cafeButton.style.color = "#000";
-  }
-});
-
-const receptionButton = document.getElementById('reception-btn') as HTMLButtonElement;
-let receptionActive = false;
-
-receptionButton.addEventListener("click", () => {
-  const receptionSpaces = mapData.getByType("space").filter(space => 
-    space.name.toLowerCase().includes("reception")
+  const lifts = mapData.getByType("space").filter((space) => 
+    space.name.toLowerCase().includes("elevator") || 
+    space.name.toLowerCase().includes("lifts") 
   );
 
-  receptionSpaces.forEach(reception => {
-    if (!receptionActive) {
-      const originalColor = mapView.getState(reception)?.color;
-      if (originalColor) {
-        originalColors.set(reception.id, originalColor);
+    lifts.forEach((lift) => {
+      if (!liftsHighlighted) {
+        const originalColor = mapView.getState(lift)?.color;
+        if (originalColor) {
+          originalColors.set(lift.id, originalColor);
+        }
+        mapView.updateState(lift, {
+          color: "#7393B3",
+        });
+      } else {
+        const originalColor = originalColors.get(lift.id);
+        mapView.updateState(lift, {
+          color: originalColor,
+        });
       }
-      mapView.updateState(reception, {
-        color: "#7393B3"
-      });
+    });
+
+    liftsHighlighted = !liftsHighlighted;
+    if (liftsHighlighted) {
+      accessibilityButton.style.backgroundColor = "#0f2240";
+      accessibilityButton.style.color = "#fff";
     } else {
-      const originalColor = originalColors.get(reception.id);
-      mapView.updateState(reception, {
-        color: originalColor
-      });
+      accessibilityButton.style.backgroundColor = "#fff";
+      accessibilityButton.style.color = "#000";
     }
   });
 
-  receptionActive = !receptionActive;
-  if (receptionActive) {
-    receptionButton.style.backgroundColor = "#0f2240";
-    receptionButton.style.color = "#fff";
-  } else {
-    receptionButton.style.backgroundColor = "#fff";
-    receptionButton.style.color = "#000";
-  }
-});
+  // Icons
+  const toiletButton = document.getElementById(
+    "toilet-btn"
+  ) as HTMLButtonElement;
+  let toiletActive = false;
 
+  toiletButton.addEventListener("click", () => {
+    const toilets = mapData
+      .getByType("space")
+      .filter((space) => space.name.toLowerCase().includes("toilet"));
 
+    toilets.forEach((toilets) => {
+      if (!toiletActive) {
+        const originalColor = mapView.getState(toilets)?.color;
+        if (originalColor) {
+          originalColors.set(toilets.id, originalColor);
+        }
+        mapView.updateState(toilets, {
+          color: "#7393B3",
+        });
+      } else {
+        const originalColor = originalColors.get(toilets.id);
+        mapView.updateState(toilets, {
+          color: originalColor,
+        });
+      }
+    });
+
+    toiletActive = !toiletActive;
+    if (toiletActive) {
+      toiletButton.style.backgroundColor = "#0f2240";
+      toiletButton.style.color = "#fff";
+    } else {
+      toiletButton.style.backgroundColor = "#fff";
+      toiletButton.style.color = "#000";
+    }
+  });
+
+  const cafeButton = document.getElementById("cafe-btn") as HTMLButtonElement;
+  let cafeActive = false;
+
+  cafeButton.addEventListener("click", () => {
+    const cafes = mapData
+      .getByType("space")
+      .filter((space) => space.name.toLowerCase().includes("cafe"));
+
+    cafes.forEach((cafes) => {
+      if (!cafeActive) {
+        const originalColor = mapView.getState(cafes)?.color;
+        if (originalColor) {
+          originalColors.set(cafes.id, originalColor);
+        }
+        mapView.updateState(cafes, {
+          color: "#7393B3",
+        });
+      } else {
+        const originalColor = originalColors.get(cafes.id);
+        mapView.updateState(cafes, {
+          color: originalColor,
+        });
+      }
+    });
+
+    cafeActive = !cafeActive;
+    if (cafeActive) {
+      cafeButton.style.backgroundColor = "#0f2240";
+      cafeButton.style.color = "#fff";
+    } else {
+      cafeButton.style.backgroundColor = "#fff";
+      cafeButton.style.color = "#000";
+    }
+  });
+
+  const receptionButton = document.getElementById(
+    "reception-btn"
+  ) as HTMLButtonElement;
+  let receptionActive = false;
+
+  receptionButton.addEventListener("click", () => {
+    const receptionSpaces = mapData
+      .getByType("space")
+      .filter((space) => space.name.toLowerCase().includes("reception"));
+
+    receptionSpaces.forEach((reception) => {
+      if (!receptionActive) {
+        const originalColor = mapView.getState(reception)?.color;
+        if (originalColor) {
+          originalColors.set(reception.id, originalColor);
+        }
+        mapView.updateState(reception, {
+          color: "#7393B3",
+        });
+      } else {
+        const originalColor = originalColors.get(reception.id);
+        mapView.updateState(reception, {
+          color: originalColor,
+        });
+      }
+    });
+
+    receptionActive = !receptionActive;
+    if (receptionActive) {
+      receptionButton.style.backgroundColor = "#0f2240";
+      receptionButton.style.color = "#fff";
+    } else {
+      receptionButton.style.backgroundColor = "#fff";
+      receptionButton.style.color = "#000";
+    }
+  });
 }
 
 init();
