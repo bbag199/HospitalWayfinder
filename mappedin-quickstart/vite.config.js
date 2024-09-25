d1cfb01529611d8b3950a94b1bf04543ab3a92e1
bbag199 <148782320+bbag199@users.noreply.github.com>
2024-09-25 14:48:51 +1200

    Create vite.config.js

---
+++ mappedin-quickstart/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 9000, // Adjust the limit to suppress warnings
+    rollupOptions: {
+        external: ['react', 'react-dom'], // Add other external dependencies if needed
+      },
+},
+});
